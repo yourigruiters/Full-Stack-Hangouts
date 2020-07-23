33597c917236ba1d9df12b0deb141f1c1ba45252
SynchronDEV <chris.davidov@gmail.com>
2020-07-23 14:29:45 +0200

    Fix: Implemented chat functionality. Clients can now chat with eachother

--- client/src/modules/chatsingle/ChatSingle.view.jsx
+++ client/src/modules/chatsingle/ChatSingle.view.jsx
@@ -4,26 +4,44 @@ import "./ChatSingle.view.scss";
 import { Link } from "react-router-dom";
 
 const ChatSingle = ({ socket, match }) => {
-	const [messages, setMessages] = React.useState([]);
+  const [messages, setMessages] = React.useState([]);
+  const [chatInput, setChatInput] = React.useState("");
+  const roomName = match.params.roomName;
 
 	React.useEffect(() => {
-		const roomName = match.params.roomName;
 
 		socket.emit("joining_room", roomName);
 
 		socket.on("message", (messageObject) => {
+      const { user, type, message } = messageObject;
 			setMessages((prevState) => {
-				const message = {
-					name: messageObject.user,
-					message: `${messageObject.user} has ${messageObject.type} the chatroom`,
-					timestamp: new Date(),
-					type: "join",
-				};
+        const newMessage = {
+          name: user, 
+          timestamp: new Date(),
+        };
+        if(type === 'joined' || type === 'left') {
+          newMessage.message = `has ${type} the chatroom`;
+          newMessage.type = type;
+        } else if (type === 'message') {
+          newMessage.message = message;
+          newMessage.type = type;
+        }
+				
 
-				return [...prevState, message];
+				return [...prevState, newMessage];
 			});
 		});
-	}, []);
+  }, []);
+
+  const sendChatMessage = (event) => {
+    event.preventDefault();
+    const message = {
+      room: roomName,
+      message: chatInput
+    }
+    socket.emit('sending_message', message);
+    setChatInput('');
+  }
 
 	return (
 		<div className="chatsingle">
@@ -32,9 +50,13 @@ const ChatSingle = ({ socket, match }) => {
 			<div className="chat">
 				<p>MESSAGES</p>
 				{messages.map((message, index) => (
-					<div key={index}>{message.message}</div>
+					<div key={index}><p><span>{message.name}: </span>{message.message}</p></div>
 				))}
 			</div>
+      <form onSubmit={(e) => sendChatMessage(e)}>
+        <input type="text" value={chatInput} placeholder="Type something..." onChange={(e) => setChatInput(e.target.value)}/>
+        <input type="button" value="Send Message" />
+      </form>
 		</div>
 	);
 };
