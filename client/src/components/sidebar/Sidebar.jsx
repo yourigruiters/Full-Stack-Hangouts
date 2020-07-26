8ae866345af2d24f01b608989c0b4f0e66e9a9fe
Youri Gruiters <i-youri-i@hotmail.com>
2020-07-26 23:08:55 +0200

    Ui: implemented new colors, images and styling

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import "./Sidebar.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
+import { NavbarVideos, NavbarChats } from "../../icons/icons";
 
-const Sidebar = () => {
-	return (
-		<section className="sidebar">
-			<nav className="sidebar__nav">
-				<NavLink to="/dashboard/videos" exact>
-					Browse Video Rooms
-				</NavLink>
-				<NavLink to="/dashboard/chats" exact>
-					Browse Chat Rooms
-				</NavLink>
-			</nav>
-		</section>
-	);
+const Sidebar = ({ history }) => {
+  const logout = () => {
+    localStorage.removeItem("userData");
+    history.push("/");
+  };
+
+  return (
+    <section className="sidebar">
+      <nav className="sidebar__nav">
+        <NavLink to="/dashboard/videos" exact>
+          <article className="sidebar__nav__combo">
+            <NavbarVideos />
+            <p>Videos</p>
+          </article>
+        </NavLink>
+        <NavLink to="/dashboard/chats" exact>
+          <article className="sidebar__nav__combo">
+            <NavbarChats />
+            <p>Chats</p>
+          </article>
+        </NavLink>
+      </nav>
+      <div className="sidebar__logout">
+        <article
+          className="sidebar__nav__combo sidebar__nav__combo--logout"
+          onClick={logout}
+        >
+          <NavbarChats />
+          <p>Logout</p>
+        </article>
+      </div>
+    </section>
+  );
 };
 
-export default Sidebar;
+export default withRouter(Sidebar);
---
+++ client/src/components/title/Title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Title.scss";
+
+const Title = ({ children }) => {
+  return <p className="paragraph">{children}</p>;
+};
+
+export default Title;
