52aa6f5022e2b24b330ee737502a85cceeb4fb64
SynchronDEV <chris.davidov@gmail.com>
2020-07-27 13:51:33 +0200

    FIX: Added HTML and styling to VideoSingle, still WIP on CSS

--- client/src/modules/videosingle/VideoSingle.view.jsx
+++ client/src/modules/videosingle/VideoSingle.view.jsx
@@ -27,16 +27,43 @@ const VideoSingle = ({ socket, match }) => {
 	}, []);
 
 	return (
-		<div className="videosingle">
-			<h1>VideoSingle</h1>
-
-			<div className="chat">
-				<p>MESSAGES</p>
-				{messages.map((message, index) => (
-					<div key={index}>{message.message}</div>
-				))}
-			</div>
-		</div>
+		<section className="videosingle">
+			<section className="videosection">
+        <section className="videosection__header">
+        <h1>Video Room Name</h1>
+        </section>
+        {/* COMPONENT HERE */}
+        <section classname="videosection__video">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/ZLm6q1-S6gM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
+        <section className="videosection__content">
+          <section className="videosection__content__info">
+            <h2>Video Title</h2>
+          </section>
+          <section className="videosection__content__queue">
+            <section className="videosection__content__queue__header">
+              <h2>Video queue</h2>
+              <button>Add To Queue</button>
+            </section>
+            <section className="videosection__content__queue__videos">
+              <h1>Test</h1>
+              <h1>Test</h1>
+              <h1>Test</h1>
+            </section>
+          </section>
+        </section>
+        
+      </section>
+      <section className="videochatsection">
+        <div className="videochatsection__header">
+          <h1>Chat</h1>
+        </div>
+        <div className="videochatsection__chat">
+          <h1>testing</h1>
+        </div>
+        {/* chat component here */}
+      </section>
+		</section>
 	);
 };
 
